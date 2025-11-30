a5eac53d0d9e4b103243ea3ac6de65e88a56b059
eldritchtools <eldritch.tools@gmail.com>
2025-11-30 16:40:20 +0800

    Fix auth process, move keywords to title in build entry

--- src/app/components/BuildEntry.js
+++ src/app/components/BuildEntry.js
@@ -27,6 +27,12 @@ export default function BuildEntry({ build, minified }) {
     return (
         <div style={{ width: "720px", display: "flex", flexDirection: "column", border: "1px #777 solid", borderRadius: "1rem", padding: "1rem", boxSizing: "border-box", textAlign: "left" }}>
             <Link href={`/builds/${build.id}`}>
+                <h2 style={{ display: "flex", fontSize: "1.2rem", fontWeight: "bold", alignItems: "center", marginBottom: "0.5rem" }}>
+                    <div style={{ display: "flex", gap: "0.2rem" }}>
+                        {build.keyword_ids.map(id => <KeywordIcon key={id} id={keywordIdMapping[id]} />)}
+                    </div>
+                    {build.title}
+                </h2>
                 <h2 style={{ fontSize: "1.2rem", fontWeight: "bold", marginTop: "0.2rem", marginBottom: "0.5rem" }}>{build.title}</h2>
             </Link>
             <div style={{ fontSize: "0.8rem", marginBottom: "0.5rem", color: "#ddd" }}>
@@ -34,14 +40,6 @@ export default function BuildEntry({ build, minified }) {
             </div>
             <div style={{ display: "flex", flexDirection: "row", marginBottom: "0.5rem" }}>
                 <IdentityImgSpread identityIds={build.identity_ids} scale={.375} />
-                <div style={{ display: "flex", flexDirection: "column", alignItems: "center", flex: 1 }}>
-                    <div style={{ marginBottom: "0.5rem" }}>
-                        Keywords
-                    </div>
-                    <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 32px)" }}>
-                        {build.keyword_ids.map((id, i) => <KeywordIcon key={i} id={keywordIdMapping[id]} />)}
-                    </div>
-                </div>
             </div>
             <div style={{ display: "flex", flexDirection: "row", gap: "0.5rem", alignItems: "center" }}>
                 Tags: {build.tags.map((t, i) => t ? <Tag key={i} tag={t} /> : null)}

579dadb66cade6a93a75769709fe2d4999352719
eldritchtools <eldritch.tools@gmail.com>
2025-12-10 03:54:47 +0800

    Removing CSP for now due to issues with GA and CF

--- next.config.mjs
+++ next.config.mjs
@@ -7,34 +7,34 @@ const nextConfig = {
     env: {
         NEXT_PUBLIC_LAST_UPDATED: new Date().toISOString(),
     },
-    async headers() {
-        // Only apply CSP in production
-        if (process.env.NODE_ENV !== "production") {
-            return [];
-        }
+    // async headers() {
+    //     // Only apply CSP in production
+    //     if (process.env.NODE_ENV !== "production") {
+    //         return [];
+    //     }
 
-        const csp = [
-            "default-src 'self'",
-            "img-src 'self' https: data:",
-            "style-src 'self' 'unsafe-inline'",
-            "font-src 'self' https:",
-            "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
-            "connect-src 'self' https:",
-            "frame-ancestors 'none'",
-        ].join("; ");
+    //     const csp = [
+    //         "default-src 'self'",
+    //         "img-src 'self' https: data:",
+    //         "style-src 'self' 'unsafe-inline'",
+    //         "font-src 'self' https:",
+    //         "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+    //         "connect-src 'self' https:",
+    //         "frame-ancestors 'none'",
+    //     ].join("; ");
 
-        return [
-            {
-                source: "/(.*)",
-                headers: [
-                    {
-                        key: "Content-Security-Policy",
-                        value: csp,
-                    },
-                ],
-            },
-        ];
-    },
+    //     return [
+    //         {
+    //             source: "/(.*)",
+    //             headers: [
+    //                 {
+    //                     key: "Content-Security-Policy",
+    //                     value: csp,
+    //                 },
+    //             ],
+    //         },
+    //     ];
+    // },
 };
 
 export default nextConfig;

bfd1d9533443ea14e1d6913702eb05f906faa498
eldritchtools <eldritch.tools@gmail.com>
2025-12-14 04:20:39 +0800

    Center align filters on identity and ego pages when wrapping

--- src/app/identities/page.js
+++ src/app/identities/page.js
@@ -380,7 +380,7 @@ export default function Identities() {
     }, [identities, identitiesLoading, statuses, statusesLoading]);
 
     return <div style={{ display: "flex", flexDirection: "column", maxHeight: "100%", width: "100%", gap: "1rem", alignItems: "center" }}>
-        <div style={{ display: "flex", gap: "2rem", alignItems: "center", flexWrap: "wrap" }}>
+        <div style={{ display: "flex", gap: "2rem", alignItems: "center", flexWrap: "wrap", justifyContent: "center" }}>
             <div style={{ display: "grid", gridTemplateColumns: "repeat(2, auto)", alignItems: "center", justifyContent: "center", gap: "0.5rem" }}>
                 <span style={{ textAlign: 'end' }}>Search:</span>
                 <input value={searchString} onChange={e => setSearchString(e.target.value)} />

eb7c705c5066cb332c62b86ae8f1068aa8023f1d
eldritchtools <eldritch.tools@gmail.com>
2025-12-13 17:41:45 +0800

    Change preview of markdown editor

--- src/app/components/MarkdownEditor.js
+++ src/app/components/MarkdownEditor.js
@@ -1,7 +1,8 @@
 "use client";
 
 import Link from "next/link";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import MarkdownRenderer from "./MarkdownRenderer";
 
 import "easymde/dist/easymde.min.css";
 import "codemirror/theme/monokai.css";
@@ -11,6 +12,9 @@ import dynamic from "next/dynamic";
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), { ssr: false });
 
 export default function MarkdownEditor({ value, onChange, placeholder }) {
+    const [mode, setMode] = useState("edit");
+    const modeStyle = { fontSize: "1rem", fontWeight: "bold", cursor: "pointer", transition: "all 0.2s" };
+
     const mdeOptions = useMemo(() => {
         return {
             theme: "monokai",
@@ -22,7 +26,7 @@ export default function MarkdownEditor({ value, onChange, placeholder }) {
                 "bold", "italic", "heading", "|",
                 "quote", "unordered-list", "ordered-list", "|",
                 "link", "image", "|",
-                "preview", "guide"
+                "guide"
             ],
             height: "auto",
             minHeight: "10rem",
@@ -31,9 +35,16 @@ export default function MarkdownEditor({ value, onChange, placeholder }) {
     }, [placeholder]);
 
     return <div style={{ display: "flex", flexDirection: "column", gap: "0.1rem" }}>
-        <SimpleMDE value={value} onChange={onChange} options={mdeOptions} />
-        <div style={{ fontSize: "0.8rem" }}>You can reference things like statuses and keywords with tokens like {"{keyword:Burn}"}.&nbsp;
-            <Link href={"/markdown-tokens"} target="_blank" rel="noopener noreferrer">Click here</Link> for more details.
-            These tokens won&apos;t work in the preview, but will work once posted.</div>
+        <div style={{ display: "flex", marginBottom: "0.5rem", gap: "1rem" }}>
+            <div style={{ ...modeStyle, color: mode === "edit" ? "#ddd" : "#777" }} onClick={() => setMode("edit")}>Edit</div>
+            <div style={{ ...modeStyle, color: mode === "preview" ? "#ddd" : "#777" }} onClick={() => setMode("preview")}>Preview</div>
+        </div>
+        {mode === "edit" ? <SimpleMDE value={value} onChange={onChange} options={mdeOptions} /> : null}
+        {mode === "preview" ? <MarkdownRenderer content={value} /> : null}
+        {mode === "edit" ?
+            <div style={{ fontSize: "0.8rem" }}>You can reference things like statuses and keywords with tokens like {"{keyword:Burn}"}.&nbsp;
+                <Link href={"/markdown-tokens"} target="_blank" rel="noopener noreferrer">Click here</Link> for more details.</div>
+            : null
+        }
     </div>
 }
